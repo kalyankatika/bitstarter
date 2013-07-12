bf3cb49c9210c9f246487d4858dd2e298af33558
Kalyan Katika <kalyankatika@gmail.com>
2013-07-12 04:51:38 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ var fs = require('fs');
+ var content = fs.readFileSync('index.html', 'utf8');
+ response.send(content.toString());
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
